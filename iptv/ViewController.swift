74219fcee75fed5e01f7c757032097b837024e8e
Artem Stepuk <artem.stepuk@gmail.com>
2018-06-08 21:49:21 +0300

    Initial Commit

---
+++ iptv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iptv
+//
+//  Created by Artem Stepuk on 6/8/18.
+//  Copyright © 2018 Artem Stepuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
